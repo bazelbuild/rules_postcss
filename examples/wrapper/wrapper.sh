1c53d218ad4efdd5255e422a440dd653ef165489
Richard Wang <richardwa@google.com>
2020-05-14 18:30:20 +1000

    Add example and test for wrapper

---
+++ examples/wrapper/wrapper.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright 2020 The Bazel Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "argv is: $@"
+
+echo "wrapped executable is: $1"
+
+echo "sliced argv is: ${@:2}"
+
+echo "running wrapped executable"
+
+$1 ${@:2}
+
+echo "finished running wrapped executable"
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--outCssFile="* ]]; then
+        outCssFile="${1/--outCssFile=/}"
+    fi
+    shift
+done
+
+echo "outCssFile is $outCssFile"
+
+echo "/*hello!*/" >> $outCssFile
