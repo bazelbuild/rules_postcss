85a36d7752d62bc1eb87ce5657ce920b04621e32
Richard Wang <richardwa@google.com>
2020-11-09 13:44:27 +1100

    Move logic from global scope into a function (#58)

--- internal/runner.js
+++ internal/runner.js
@@ -26,9 +26,6 @@ const path = require('path');
 const postcss = require('postcss');
 const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
 
-const args = minimist(process.argv.slice(2));
-const cwd = process.cwd();
-
 /**
  * Returns the argument named `name` as an array.
  *
@@ -36,92 +33,107 @@ const cwd = process.cwd();
  * arrays. This ensures a consistent representation if the same arg is passed
  * zero or one times.
  */
-function argAsArray(name) {
+function argAsArray(args, name) {
   const value = args[name];
   if (!value) return [];
   if (typeof value === 'string') return [value];
   return value;
 }
 
-const data = argAsArray('data');
-for (const pair of argAsArray('namedData')) {
-  const [name, value] = pair.split(':');
-  (data[name] = data[name] || []).push(value);
+function compile(rawArgs) {
+  const args = minimist(rawArgs);
+  const cwd = process.cwd();
+
+  const data = argAsArray(args, 'data');
+  for (const pair of argAsArray(args, 'namedData')) {
+    const [name, value] = pair.split(':');
+    (data[name] = data[name] || []).push(value);
+  }
+
+  // These variables are documented in `postcss_binary`'s docstring and are fair
+  // game for plugin configuration to read.
+  const bazel = {
+    binDir: args.binDir,
+    data: data,
+    additionalOutputs: argAsArray(args, 'additionalOutputs'),
+  };
+
+  const cssString = fs.readFileSync(args.cssFile, 'utf8');
+
+  const options = {
+    // The path of the input CSS file.
+    from: args.cssFile,
+    // The path of the output CSS file.
+    to: args.outCssFile,
+    map: args.sourcemap
+        ? {
+            // Don't output the source map inline, we want it as a separate file.
+            inline: false,
+            // Whether to add (or modify, if already existing) the
+            // sourceMappingURL comment in the output .css to point to the output
+            // .css.map.
+            annotation: true,
+          }
+        : false,
+  };
+
+  // Get absolute output paths before we change directory.
+  const outCssPath = path.join(cwd, args.outCssFile);
+  const outCssMapPath =
+      args.outCssMapFile ? path.join(cwd, args.outCssMapFile) : null;
+
+  // We use two parallel arrays of PostCSS plugin requires => strings of JS args.
+  // To use in PostCSS, convert these into the actual plugin instances.
+  const pluginRequires = argAsArray(args, 'pluginRequires');
+  const pluginArgs = argAsArray(args, 'pluginArgs');
+  const pluginInstances = pluginRequires.map(
+      (nodeRequire, i) => {
+        // Try and resolve this plugin as a module identifier.
+        let plugin;
+        try {
+          plugin = require(nodeRequire);
+        } catch { }
+
+        // If it fails, use the runfile helper in case it's a workspace file.
+        if (!plugin) {
+          try {
+            plugin = require(runfiles.resolve(nodeRequire));
+          } catch { }
+        }
+
+        // If that still fails, throw an error.
+        if (!plugin) {
+          const e = new Error(
+              `could not resolve plugin with node require ${nodeRequire}`);
+          e.code = 'MODULE_NOT_FOUND';
+          throw e;
+        }
+
+        return plugin.apply(this, eval(pluginArgs[i]));
+      });
+
+  return postcss(pluginInstances)
+      .process(cssString, options)
+      .then(
+          result => {
+            fs.writeFileSync(outCssPath, result.css);
+            if (args.sourcemap && result.map) {
+              fs.writeFileSync(outCssMapPath, result.map);
+            }
+            return true;
+          },
+          e => {
+            console.warn('Error in processing. Args:', args);
+            console.warn('Error:', e);
+            return false;
+          });
 }
 
-// These variables are documented in `postcss_binary`'s docstring and are fair
-// game for plugin configuration to read.
-const bazel = {
-  binDir: args.binDir,
-  data: data,
-  additionalOutputs: argAsArray('additionalOutputs'),
-};
-
-const cssString = fs.readFileSync(args.cssFile, 'utf8');
-
-const options = {
-  // The path of the input CSS file.
-  from: args.cssFile,
-  // The path of the output CSS file.
-  to: args.outCssFile,
-  map: args.sourcemap
-      ? {
-          // Don't output the source map inline, we want it as a separate file.
-          inline: false,
-          // Whether to add (or modify, if already existing) the
-          // sourceMappingURL comment in the output .css to point to the output
-          // .css.map.
-          annotation: true,
-        }
-      : false,
-};
-
-// Get absolute output paths before we change directory.
-const outCssPath = path.join(cwd, args.outCssFile);
-const outCssMapPath =
-    args.outCssMapFile ? path.join(cwd, args.outCssMapFile) : null;
-
-// We use two parallel arrays of PostCSS plugin requires => strings of JS args.
-// To use in PostCSS, convert these into the actual plugin instances.
-const pluginRequires = argAsArray('pluginRequires');
-const pluginArgs = argAsArray('pluginArgs');
-const pluginInstances = pluginRequires.map(
-    (nodeRequire, i) => {
-      // Try and resolve this plugin as a module identifier.
-      let plugin;
-      try {
-        plugin = require(nodeRequire);
-      } catch { }
-
-      // If it fails, use the runfile helper in case it's a workspace file.
-      if (!plugin) {
-        try {
-          plugin = require(runfiles.resolve(nodeRequire));
-        } catch { }
+const args = process.argv.slice(2);
+compile(args).then(
+    success => {
+      // Arbitrary exit code for failure, otherwise we exit succesfully.
+      if (!success) {
+        process.exit(2);
       }
-
-      // If that still fails, throw an error.
-      if (!plugin) {
-        const e = new Error(
-            `could not resolve plugin with node require ${nodeRequire}`);
-        e.code = 'MODULE_NOT_FOUND';
-        throw e;
-      }
-
-      return plugin.apply(this, eval(pluginArgs[i]));
     });
-
-postcss(pluginInstances)
-    .process(cssString, options)
-    .then(
-        result => {
-          fs.writeFileSync(outCssPath, result.css);
-          if (args.sourcemap && result.map) {
-            fs.writeFileSync(outCssMapPath, result.map);
-          }
-        },
-        e => {
-          console.warn('Error in processing. Args:', args);
-          console.warn('Error:', e);
-          process.exit(2);
-        });
